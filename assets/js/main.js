b681c425fa765b6eaea3cdf007b522dccfb79998
Luiza Asth <168010169+luxnfrj@users.noreply.github.com>
2024-11-27 23:33:08 +0000

    Finalizando Projeto

---
+++ assets/js/main.js
@@ -0,0 +1,56 @@
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+
+const maxRecords = 151
+const limit = 10
+let offset = 0;
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon ${pokemon.type}" data-name="${pokemon.name}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+
+                <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+pokemonList.addEventListener('click', (event) => {
+    const listItem = event.target.closest('li.pokemon');
+    if (listItem) {
+        const name = listItem.dataset.name;
+        window.location.href = `detalhe.html?name=${name}`; // Corrigido o caminho do arquivo de destino
+    }
+});
+
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemonToLi).join('')
+        pokemonList.innerHTML += newHtml
+    })
+}
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecordsWithNexPage = offset + limit
+
+    if (qtdRecordsWithNexPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
+})
---
+++ assets/js/pokemon-model.js
@@ -0,0 +1,15 @@
+
+class Pokemon {
+    number;
+    name;
+    type;
+    types = [];
+    photo;
+    height;
+    weight;
+    abilities = [];
+    moves = [];
+    stats = [];
+    stat;
+    indiceStat = [];
+}
